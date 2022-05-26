914d0d77b756077d4308db30229f9a8fa496a5f2
kaan <aknckaan1@gmail.com>
2022-05-27 05:44:15 +0900

    added dali

--- dataloader.py
+++ dataloader.py
@@ -5,51 +5,40 @@ from torchvision.io import read_video
 import kornia.augmentation as K
 import cv2
 import torch
+import nvidia.dali as dali
+from nvidia.dali import pipeline_def
+import nvidia.dali.fn as fn
+import nvidia.dali.types as types
 
+def build_pipeline( video_dir, annot_path):
+    video_label_map = []
+    label_int = []
+    i = 0
+    with open(annot_path, newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        for row in spamreader:
+            i+=1
+            if i ==1:
+                continue
 
-class VideoLoader(Dataset):
-    def __init__(self, video_dir, annot_path):
-        super().__init__()
-        self.video_dir = video_dir
-        self.video_label_map = []
-        self.label_int = []
-        i = 0
-        with open(annot_path, newline='') as csvfile:
-            spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
-            for row in spamreader:
-                i+=1
-                if i ==1:
-                    continue
+            if not row[1] in label_int:
+                label_int += [row[1]]
 
-                if not row[1] in self.label_int:
-                    self.label_int += [row[1]]
+            video_label_map += [[row[0], label_int.index(row[1])]]
 
-                self.video_label_map += [[row[0], self.label_int.index(row[1])]]
+    file_names = list(map(lambda x: "dataset/train/"+x[0], video_label_map))
+    file_labels = list(map(lambda x: x[1], video_label_map))
+    sequence_length = 8
+    initial_prefetch_size = 16
+    batch_size = 2
 
-    def __len__(self):
-        return len(self.video_label_map)
+    @pipeline_def
+    def GetMnistPipeline(device):
+        video, labels = fn.readers.video(device="gpu", filenames=file_names, labels=file_labels, sequence_length=sequence_length,
+                                        random_shuffle=True, initial_fill=initial_prefetch_size)
+        
+        
+        return video, labels
 
-    def read_video(self, dir):
-        video = []
-        cap = cv2.VideoCapture(dir)
-        while(cap.isOpened()):
-            # Capture frame-by-frame
-            ret, frame = cap.read()
-            if ret == True:
-                video.append(frame)
-
-        video = torch.Tensor(video)
-        return video
-
-
-    def __getitem__(self, idx):
-        vid_name, label = self.video_label_map[idx]
-        img_dir = Path(self.video_dir) / vid_name
-        vid = read_video(str(img_dir))[0] / 255
-        vid = K.Resize([224, 224])(vid)
-
-        return vid, label
-
-vl = VideoLoader("dataset/train", "dataset/train.csv")
-vid, label = vl.__getitem__(0)
-print(vid[1].shape)
+    pipe = GetMnistPipeline("gpu",num_threads=1, batch_size=batch_size, device_id=0)
+    return pipe
--- training_loop.py
+++ training_loop.py
@@ -1,7 +1,19 @@
 from network import VideoClassifier
 import pytorch_lightning as pl
 import torch
-
+import nvidia.dali as dali
+from nvidia.dali import pipeline_def
+import nvidia.dali.fn as fn
+import nvidia.dali.types as types
+from nvidia.dali.plugin.pytorch import DALIClassificationIterator, LastBatchPolicy
+from dataloader import build_pipeline
+import os
+batch_size=2
+sequence_length=8
+initial_prefetch_size=16
+video_directory = "dataset/train"
+n_iter=6
+# Path to MNIST dataset
 
 class VideoClassificationModel(pl.LightningModule):
     def __init__(self, transforms):
@@ -29,3 +41,26 @@ class VideoClassificationModel(pl.LightningModule):
         x = self.forward(x)
         loss = torch.nn.Functional.cross_entropy_loss(x, y)
         self.log('val_loss', loss)
+
+    def setup(self, stage=None):
+        device_id = self.local_rank
+        shard_id = self.global_rank
+        num_shards = self.trainer.world_size
+        pipe = build_pipeline("dataset/train", "dataset/train.csv")
+
+        class LightningWrapper(DALIClassificationIterator):
+            def __init__(self, *kargs, **kvargs):
+                super().__init__(*kargs, **kvargs)
+
+            def __next__(self):
+                out = super().__next__()
+                # DDP is used so only one pipeline per process
+                # also we need to transform dict returned by DALIClassificationIterator to iterable
+                # and squeeze the lables
+                out = out[0]
+                return [out[k] if k != "label" else torch.squeeze(out[k]) for k in self.output_map]
+
+        self.train_loader = LightningWrapper(pipe, reader_name="Reader", last_batch_policy=LastBatchPolicy.PARTIAL, auto_reset=True)
+
+    def train_dataloader(self):
+        return self.train_loader
